5c779a6652c56aa76b7f56c6335fd9875a5b29e4
Alphaandsew <alfonso.martinez@shrikefound.com>
2021-02-23 14:51:36 -0800

    added remove command

--- commands/remove.js
+++ commands/remove.js
@@ -1,7 +1,7 @@
-const { findSuit,cheat } = require('../dealer.js');
+const { findSuit } = require('../dealer.js');
 module.exports = {
-  name: 'cheat',
-  description: 'cheats a card with the given value from your control hand. Use `!cheat [card value]`',
+  name: 'remove',
+  description: 'removes a card with the given value from your twist deck. Use `!remove [value] [suit]`',
   async execute(bot, message, args) {
     
     if (!message.channel.guild) {
@@ -10,7 +10,7 @@ module.exports = {
     }
     
     
-    let cheatedCard;
+    let removedCard;
     let value = args.join(" ").match(/\d+/)
     if (value) {
       value = Number(value[0])
@@ -25,19 +25,11 @@ module.exports = {
     const deckString = `channels/${guildID}/users/${userID}/twist_deck`
     let db = admin.database();
     deckRef = db.ref(deckString)
-    deckRef.once("value", (snapshot) => {
-      twistDeck = snapshot.val();
-      cheatedCard = cheat(twistDeck, value)
-      console.log(cheatedCard);
-      deckRef.update(twistDeck);
-    }).then(() => {
-      if (cheatedCard) {
-        message.reply(`${message.author.username} cheated with a ${JSON.stringify(cheatedCard)}`)        
-      } else {
-        message.reply("sorry, couldn't find that card to cheat with.")
-      }
-
-    })
+    deckRef.orderByChild("value").equalTo(value).once("value", (snapshot) => {
+      console.log("snapshot: ", snapshot)
+      console.log("snapshot val: ", snapshot.val())
+      console.log("snapshot key: ", snapshot.key)
+    });
 
   }
 }
