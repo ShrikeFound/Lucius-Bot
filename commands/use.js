e3657301f2e7c7feba73136cead58068885984cf
Alphaandsew <alfonso.martinez@shrikefound.com>
2021-03-01 15:41:50 -0800

    Update use.js

--- commands/use.js
+++ commands/use.js
@@ -3,14 +3,17 @@ module.exports = {
   description: 'uses a character sheet created through the web app.',
   async execute(bot, message, args) {
     
+    if (!message.channel.guild) {
+      message.reply("Please try this command in a text channel.")
+      return
+    }
     const characterID = args[0]
     const admin = require('firebase-admin');
     const guildID = message.channel.guild.id
     const userID = message.author.id
     let db = admin.database();
-    let characterRef = db.ref('characters/'+characterID)
-    message.reply(args[0]);
-    characterRef.child(userID).set({
+    let characterRef = db.ref('characters/' + characterID)
+    useData = {
       approved: false,
       active: false,
       ignored: false,
@@ -19,7 +22,10 @@ module.exports = {
       guild_user: guild_ID+"_"+userID,
       guild_name: message.channel.guild.name,
       nick_name: message.channel.guild.name
-    })
+    }
+    console.log(useData);
+    message.reply(args[0]);
+    characterRef.child(userID).set(useData)
 
     console.log("no errors using the use command.");
   }
